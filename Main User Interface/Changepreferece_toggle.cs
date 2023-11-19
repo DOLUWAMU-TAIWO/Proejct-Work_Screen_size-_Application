8fa0d791752ae252f14b05dfac21e8648348cfc2
DOLUWAMU-TAIWO <91460092+DOLUWAMU-TAIWO@users.noreply.github.com>
2023-11-19 07:24:09 +0100

    Add files via upload

---
+++ Main User Interface/Changepreferece_toggle.cs
@@ -0,0 +1,42 @@
+using UnityEngine.UIElements;
+using UnityEngine;
+
+public class ImageDisplayPreference : MonoBehaviour
+{
+    public Input_Variables cube;
+    public UIDocument uiDocument; // Drag your UIDocument component in the Inspector
+
+    private Toggle autoToggle;
+    private Toggle displayToggle;
+    private Toggle projectionToggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the root VisualElement
+        VisualElement root = uiDocument.rootVisualElement;
+
+        // Get references to toggle elements
+        autoToggle = root.Q<Toggle>("AUTO");
+        displayToggle = root.Q<Toggle>("DISPLAY");
+        projectionToggle = root.Q<Toggle>("PROJEKTION");
+
+        // Subscribe to the valueChanged event for each toggle
+        autoToggle.RegisterValueChangedCallback(evt => OnToggleValueChanged(evt, "AUTO"));
+        displayToggle.RegisterValueChangedCallback(evt => OnToggleValueChanged(evt, "DISPLAY"));
+        projectionToggle.RegisterValueChangedCallback(evt => OnToggleValueChanged(evt, "PROJEKTION"));
+    }
+
+    // Callback method when any toggle value changes
+    private void OnToggleValueChanged(ChangeEvent<bool> evt, string toggleName)
+    {
+        // Assuming you have a reference to the Input_Variables script
+        Input_Variables inputVariables = FindObjectOfType<Input_Variables>();
+
+        if (inputVariables != null)
+        {
+            // Call the change_preference function in the Input_Variables script
+            inputVariables.change_preference(evt.newValue, toggleName);
+        }
+    }
+}
