4d6c39af83b61490a7ab36b0d2d561f5de343b17
DOLUWAMU-TAIWO <91460092+DOLUWAMU-TAIWO@users.noreply.github.com>
2023-12-13 10:02:55 +0100

    Update View_Mode.cs

--- View_Mode/View_Mode.cs
+++ View_Mode/View_Mode.cs
@@ -4,17 +4,17 @@ using System.Collections.Generic; // Needed for Dictionary
 
 public class ViewMode : MonoBehaviour
 {
-    public UIDocument uiDocument; // Reference to the UIDocument that contains your UI
-    public GameObject[] gameObjectsToToggle; // Array for the GameObjects you want to toggle
+    public UIDocument uiDocument; // Reference to the UIDocument containing the UI.
+    public GameObject[] gameObjectsToToggle; // Array of GameObjects to be toggled.
 
-    private Dictionary<GameObject, float> originalOpacities = new Dictionary<GameObject, float>();
-    private Button viewModeButton;
-    private VisualElement rootVisualElement;
-    private bool isUIVisible = true; // Track the visibility state of the UI
+    private Dictionary<GameObject, float> originalOpacities = new Dictionary<GameObject, float>(); // Stores original opacity values of GameObjects.
+    private Button viewModeButton; // Button to toggle view mode.
+    private VisualElement rootVisualElement; // Root visual element of the UI.
+    private bool isUIVisible = true; // Flag to track UI visibility.
 
     private void Start()
     {
-        // Store the original opacity of each GameObject
+        // Initialize original opacity values for each GameObject.
         foreach (GameObject go in gameObjectsToToggle)
         {
             if (go != null)
@@ -22,11 +22,13 @@ public class ViewMode : MonoBehaviour
                 Renderer renderer = go.GetComponent<Renderer>();
                 if (renderer != null)
                 {
+                    // Store the original opacity if a Renderer is present.
                     originalOpacities[go] = renderer.material.color.a;
                 }
                 else
                 {
-                    originalOpacities[go] = 1f; // If no Renderer, default to fully opaque
+                    // Default to fully opaque if no Renderer is found.
+                    originalOpacities[go] = 1f;
                 }
             }
         }
@@ -34,30 +36,37 @@ public class ViewMode : MonoBehaviour
 
     private void OnEnable()
     {
-        rootVisualElement = uiDocument.rootVisualElement; // Get the root visual element directly
+        // Get the root visual element from the UI document.
+        rootVisualElement = uiDocument.rootVisualElement;
+        // Find the view mode button in the UI.
         viewModeButton = rootVisualElement.Q<Button>("ViewModeButton");
 
+        // Check if the view mode button is found.
         if (viewModeButton == null)
         {
+            // Log an error if the button is not found and exit the method.
             Debug.LogError("ViewModeButton not found!");
             return;
         }
 
+        // Subscribe to the click event of the view mode button.
         viewModeButton.clicked += OnViewModeButtonClicked;
     }
 
     private void Update()
     {
-        // Check if the Escape key is pressed and UI is invisible
+        // Toggle UI and GameObject visibility if Escape is pressed and the UI is not visible.
         if (Input.GetKeyDown(KeyCode.Escape) && !isUIVisible)
         {
-            ToggleUIVisibility(true); // Make the UI visible
-            ToggleObjectsVisibility(true); // Make GameObjects visible again
+            // Make UI and GameObjects visible.
+            ToggleUIVisibility(true);
+            ToggleObjectsVisibility(true);
         }
     }
 
     private void ToggleObjectsVisibility(bool visible)
     {
+        // Iterate through each GameObject to toggle its visibility.
         foreach (GameObject go in gameObjectsToToggle)
         {
             if (go != null)
@@ -65,11 +74,12 @@ public class ViewMode : MonoBehaviour
                 Renderer renderer = go.GetComponent<Renderer>();
                 if (renderer != null)
                 {
+                    // Set the opacity of the GameObject based on the 'visible' parameter.
                     Color color = renderer.material.color;
-                    color.a = visible ? originalOpacities[go] : 0.0f; // Restore to original opacity or make transparent
+                    color.a = visible ? originalOpacities[go] : 0.0f; // Use original opacity or make transparent.
                     renderer.material.color = color;
                 }
-                // For non-renderer objects, use SetActive (if needed, uncomment the line below)
+                // Uncomment the below line to toggle active state for non-renderer objects.
                 // go.SetActive(visible);
             }
         }
@@ -77,26 +87,33 @@ public class ViewMode : MonoBehaviour
 
     private void ToggleUIVisibility(bool visible)
     {
-        rootVisualElement.style.opacity = visible ? 1 : 0; // Set UI opacity to 0 to hide, 1 to show
-        isUIVisible = visible; // Update the visibility state
+        // Set the opacity of the root visual element to show or hide the UI.
+        rootVisualElement.style.opacity = visible ? 1 : 0;
+        // Update the flag to track the current visibility state of the UI.
+        isUIVisible = visible;
     }
 
     private void OnViewModeButtonClicked()
     {
+        // Toggle the visibility state based on the current UI visibility.
         bool newVisibility = !isUIVisible;
-        ToggleUIVisibility(newVisibility); // Toggle UI based on current state
-        ToggleObjectsVisibility(newVisibility); // Toggle GameObjects based on the new UI state
+        // Apply the new visibility state to UI and GameObjects.
+        ToggleUIVisibility(newVisibility);
+        ToggleObjectsVisibility(newVisibility);
     }
 
     private void OnDisable()
     {
+        // Unsubscribe from the click event of the view mode button when the script is disabled.
         if (viewModeButton != null)
         {
             viewModeButton.clicked -= OnViewModeButtonClicked;
         }
     }
+
     public void ToggleUIAndObjects(bool activate)
     {
+        // Public method to toggle both UI and GameObject visibility.
         ToggleUIVisibility(activate);
         ToggleObjectsVisibility(activate);
     }
