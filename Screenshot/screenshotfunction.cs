6bf93307f620c7ea813855ee432aec7636a2e768
DOLUWAMU-TAIWO <91460092+DOLUWAMU-TAIWO@users.noreply.github.com>
2023-12-13 09:58:50 +0100

    Update screenshotfunction.cs

--- Screenshot/screenshotfunction.cs
+++ Screenshot/screenshotfunction.cs
@@ -3,29 +3,30 @@ using System.IO;
 
 public class Screenshottaker : MonoBehaviour
 {
-    public string screenshotFolder = "Screenshots";
-    public int screenshotCount = 0;
+    public string screenshotFolder = "Screenshots"; // Folder name where screenshots will be saved.
+    public int screenshotCount = 0; // Counter for the number of screenshots taken.
 
-    private string ScreenshotPath => Path.Combine(Application.persistentDataPath, screenshotFolder);
+    private string ScreenshotPath => Path.Combine(Application.persistentDataPath, screenshotFolder); // Full path for the screenshot folder.
 
     private void Start()
     {
-        // Create the screenshot folder if it doesn't exist
+        // Check if the screenshot folder exists; if not, create it.
         if (!Directory.Exists(ScreenshotPath))
         {
-            Directory.CreateDirectory(ScreenshotPath);
+            Directory.CreateDirectory(ScreenshotPath); // Create the screenshot folder.
         }
     }
 
     public void OnButtonClick()
     {
-        // Generate screenshot filename
+        // Generate the filename for the screenshot with a count and timestamp.
         string fileName = $"screenshot_{screenshotCount++}_{System.DateTime.Now:yyyyMMdd_HHmmss}.png";
-        string filePath = Path.Combine(ScreenshotPath, fileName);
+        string filePath = Path.Combine(ScreenshotPath, fileName); // Full file path for the screenshot.
 
-        // Take the screenshot
+        // Capture the screenshot and save it to the specified path.
         ScreenCapture.CaptureScreenshot(filePath);
 
+        // Log the file path to the Unity console for confirmation or debugging.
         Debug.Log($"Screenshot saved: {filePath}");
     }
 }
